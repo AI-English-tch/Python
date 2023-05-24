b3089266ddc41cb285f21b142e96740b71048f55
He Quansen <107733553+HQsen@users.noreply.github.com>
2023-05-24 20:53:59 +0800

    Update assistant_bot.py
    prompt change

--- assistant_bot.py
+++ assistant_bot.py
@@ -12,8 +12,7 @@ class AssistantBot:
         self.event.wait()
         if self.user_text:
             context2 = {'role': 'system', 'content': f"""你是一位英语助理教授，你拥有着及其刁钻的眼光，对于语法、词汇等错误有极高的敏感度。现在请你扮演英语助理教授的角色。No matter what language I use.Reply me in English.
-你的任务是分析学生提交给你的句子，如果是英文就加以纠错和点评，如果是中文就翻译为英文并放在"Original sentence:"栏目中。学生的句子：{self.user_text}
-你的输出格式必须如下三引号所示:
+你的任务是分析学生提交给你的句子，如果是英文就加以纠错和点评，你所举例的句子必须足够口语化，像native speaker一样，如果是中文就翻译为英文并放在"Original sentence:"栏目中。学生的句子：{self.user_text}。
 '''
 Original sentence:
 Grammatical errors:
